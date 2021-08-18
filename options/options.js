0f2085e74be417a9129eb944a02459a43d98bace
Crowdbotics <team@crowdbotics.com>
2021-08-18 00:10:32 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "frosty_smoke_29897Identifier",
+  url: "https://frosty_smoke_29897Identifier.botics.co",
+  api: "https://frosty_smoke_29897Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
